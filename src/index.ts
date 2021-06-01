da1b377372f1926259e45581c870ec617c349423
saemjeon <38971167+saemjeon@users.noreply.github.com>
2021-06-01 15:40:00 -0400

    Add github action

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,20 @@ export = (app: Probot) => {
     });
     await context.octokit.issues.createComment(issueComment);
   });
+
+  app.on("issue_comment.created", async (context) => {
+    const issueComment = context.issue({
+      body: "Thanks for your comment!",
+    });
+    await context.octokit.issues.createComment(issueComment);
+  });
+
+  app.on("issues.assigned", async (context) => {
+    const issueComment = context.issue({
+      body: "It is assigned.",
+    });
+    await context.octokit.issues.createComment(issueComment);
+  });
   // For more information on building apps:
   // https://probot.github.io/docs/
 
